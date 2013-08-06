ff256c39189f4a362f54109063bac2140c6e44ac
Klaus Zanders <klaus.zanders@gmail.com>
2013-08-06 20:07:17 +0200

    Little Bugfix ... Ampersands should be encoded

--- lib/gst-kitchen/flattr.rb
+++ lib/gst-kitchen/flattr.rb
@@ -2,11 +2,11 @@ module Flattr
   def Flattr.auto_submit_link(podcast, url, title, description)
     "https://flattr.com/submit/auto" +
     "?user_id=#{podcast.flattr["user_id"]}" +
-    "&url=#{CGI.escape url}" +
-    "&title=#{CGI.escape title}" +
-    "&description=#{CGI.escape description}" +
-    "&language=#{podcast.flattr["language"]}" +
-    "&tags=#{podcast.flattr["tags"].join(',')}" +
-    "&category=#{podcast.flattr["category"]}"
+    "&amp;url=#{CGI.escape url}" +
+    "&amp;title=#{CGI.escape title}" +
+    "&amp;description=#{CGI.escape description}" +
+    "&amp;language=#{podcast.flattr["language"]}" +
+    "&amp;tags=#{podcast.flattr["tags"].join(',')}" +
+    "&amp;category=#{podcast.flattr["category"]}"
   end
 end
--- spec/flattr_spec.rb
+++ spec/flattr_spec.rb
@@ -4,7 +4,7 @@ describe Flattr do
   it "creates a valid flattr auto submit url" do
     podcast = double("podcast")
     podcast.stub(:flattr).and_return({"user_id" => "gst-podcast", "language" => "de_DE", "tags" => %w(geek stammtisch podcast), "category" => "audio"})
-    expected_url = "https://flattr.com/submit/auto?user_id=gst-podcast&url=http%3A%2F%2Fwww.test.de&title=Some+random+Title&description=Some+very+random+description&language=de_DE&tags=geek,stammtisch,podcast&category=audio"
+    expected_url = "https://flattr.com/submit/auto?user_id=gst-podcast&amp;url=http%3A%2F%2Fwww.test.de&amp;title=Some+random+Title&amp;description=Some+very+random+description&amp;language=de_DE&amp;tags=geek,stammtisch,podcast&amp;category=audio"
 
     Flattr.auto_submit_link(podcast, "http://www.test.de", "Some random Title", "Some very random description").should == expected_url
   end
